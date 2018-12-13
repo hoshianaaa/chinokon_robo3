ba1714007c9a159b252a6ec4030912bdbb174372
hoshianaaa <40942409+hoshianaaa@users.noreply.github.com>
2018-12-13 16:23:56 +0900

    Add files via upload

--- Chinokon/photo_led_control.cpp
+++ Chinokon/photo_led_control.cpp
@@ -2,8 +2,9 @@
 #include "photo_led_control.h"
 
 
-void photo_led_set(){
   int led_pin[8] = {53,51,49,47,45,39,37,35};
+
+void photo_led_set(){
   for(int i=0;i<8;i++){
     pinMode(led_pin[i],OUTPUT);
   }
@@ -12,22 +13,25 @@ void photo_led_set(){
 
 void photo_led_on(int x){
 
-  int led_pin[8] = {53,51,49,47,45,39,37,35};
-  digitalWrite(led_pin[x-1],HIGH);
-  
+  digitalWrite(led_pin[x-1],HIGH); 
 }
-
 void photo_led_off(int x){
-
-  int led_pin[8] = {53,51,49,47,45,39,37,35};
-  digitalWrite(led_pin[x-1],LOW);
-  
+digitalWrite(led_pin[x-1],LOW);
 }
 
-void photo_led_loop(){
-  int led_pin[8] = {53,51,49,47,45,39,37,35};
-  for (int i=0;i<8;i++){
-	digitalWrite(led_pin[i],HIGH);
-	digitalWrite(led_pin[i-1],LOW);
-	delay(1000); 
+
+void photo_led_alloff(){
+
+  for(int i=0;i<8;i++){
+	digitalWrite(led_pin[i],LOW);
+  }
+}
+
+
+void photo_led_loop(int x){
+  for(int i=0;i<8;i++){
+    photo_led_alloff();
+    photo_led_on(i+1);
+    delay(x);
+  }
 }
--- Chinokon/photo_led_control.h
+++ Chinokon/photo_led_control.h
@@ -5,6 +5,7 @@
 void photo_led_set();
 void photo_led_on(int);
 void photo_led_off(int);
-void photo_led_loop();
+void photo_led_alloff();
+void photo_led_loop(int x);
 
 #endif
